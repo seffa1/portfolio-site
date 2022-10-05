5d6252adec3974cb4e169a1df53e8b29b20092c2
samueleffa <seffa1@gmail.com>
2022-10-05 06:41:45 -0400

    refactored section layout / heading css. cleaned up code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,92 +12,84 @@ import { Link } from "react-router-dom";
 function Projects() {
   return (
     <>
-      <section className="Project" id="Projects">
-        {/* Project One */}
-        <div className="About__background">
-          <div className="About__heading">
-            <h4 className="About__heading__number">03.</h4>
-            <h3 className="About__heading__title">Web Projects</h3>
-            <div className="About__heading__line" />
+      {/* WEB PROJECTS */}
+      {/* Header */}
+      <section className="Project SectionContainer" id="Projects">
+        <div className="SectionHeading">
+          <h4 className="SectionHeading__number">03.</h4>
+          <h3 className="SectionHeading__title">Web Projects</h3>
+          <div className="SectionHeading__line" />
+        </div>
+        <p className="SectionHeading__paragraph">
+          Click on any project to learn more.
+        </p>
+
+        {/* Project List */}
+        <div className="Project__container">
+          <div className="Project__container__img-container">
+            <img src={twitterHome} className="Project__container__img" />
+            <Link to="/twitterClone" className="Project__container__img-hover">
+              Fullstack Twitter Clone
+            </Link>
           </div>
-
-          {/* Header */}
-          <p className="About__paragraph">
-            Click on any project to learn more.
-          </p>
-
-          {/* Project Two */}
-          <div className="Project__container">
-            <div className="Project__container__img-container">
-              <img src={twitterHome} className="Project__container__img" />
-              <Link
-                to="/twitterClone"
-                className="Project__container__img-hover"
-              >
-                Fullstack Twitter Clone
-              </Link>
-            </div>
-            <div className="Project__container__img-container">
-              <img src={newsSite} className="Project__container__img" />
-              <Link to="/newsAPI" className="Project__container__img-hover">
-                Live News API
-              </Link>
-            </div>
-            <div className="Project__container__img-container">
-              <img src={diceGame} className="Project__container__img" />
-              <Link to="/diceGame" className="Project__container__img-hover">
-                React Dice Game
-              </Link>
-            </div>
-            <div className="Project__container__img-container">
-              <img src={company} className="Project__container__img" />
-              <Link to="/companyPage" className="Project__container__img-hover">
-                Company Site Remake
-              </Link>
-            </div>
-            <div className="Project__container__img-container">
-              <img src={giphy} className="Project__container__img" />
-              <Link to="/giphyAPI" className="Project__container__img-hover">
-                Gif API
-              </Link>
-            </div>
+          <div className="Project__container__img-container">
+            <img src={newsSite} className="Project__container__img" />
+            <Link to="/newsAPI" className="Project__container__img-hover">
+              Live News API
+            </Link>
+          </div>
+          <div className="Project__container__img-container">
+            <img src={diceGame} className="Project__container__img" />
+            <Link to="/diceGame" className="Project__container__img-hover">
+              React Dice Game
+            </Link>
+          </div>
+          <div className="Project__container__img-container">
+            <img src={company} className="Project__container__img" />
+            <Link to="/companyPage" className="Project__container__img-hover">
+              Company Site Remake
+            </Link>
+          </div>
+          <div className="Project__container__img-container">
+            <img src={giphy} className="Project__container__img" />
+            <Link to="/giphyAPI" className="Project__container__img-hover">
+              Gif API
+            </Link>
           </div>
         </div>
       </section>
-      <section className="Project" id="Projects">
-        {/* Project One */}
-        <div className="About__background">
-          <div className="About__heading">
-            <h4 className="About__heading__number">04.</h4>
-            <h3 className="About__heading__title">Python Projects</h3>
-            <div className="About__heading__line" />
+
+      {/* PYTHON PROJECTS */}
+      {/* Header */}
+      <section className="Project SectionContainer" id="Projects">
+        <div className="SectionHeading">
+          <h4 className="SectionHeading__number">04.</h4>
+          <h3 className="SectionHeading__title">Python Projects</h3>
+          <div className="SectionHeading__line" />
+        </div>
+        <p className="SectionHeading__paragraph">
+          Click on any project to learn more.
+        </p>
+
+        {/* Project List */}
+        <div className="Project__container">
+          <div className="Project__container__img-container">
+            <img src={woodcutter4} className="Project__container__img" />
+            <Link to="/woodcutter" className="Project__container__img-hover">
+              2D Platformer Game
+            </Link>
           </div>
-
-          {/* Header */}
-          <p className="About__paragraph">
-            Click on any project to learn more.
-          </p>
-
-          {/* Project Two */}
-          <div className="Project__container">
-            <div className="Project__container__img-container">
-              <img src={woodcutter4} className="Project__container__img" />
-              <Link to="/woodcutter" className="Project__container__img-hover">
-                2D Platformer Game
-              </Link>
-            </div>
-            <div className="Project__container__img-container">
-              <img src={fingerFlow} className="Project__container__img" />
-              <Link to="/fingerFlow" className="Project__container__img-hover">
-                2D Puzzler Game
-              </Link>
-            </div>
-            <div className="Project__container__img-container">
-              <img src={raspberryPi} className="Project__container__img" />
-              <Link to="/raspberryPI" className="Project__container__img-hover">
-                RaspberryPi Datalogger
-              </Link>
-            </div>
+          <div className="Project__container__img-container">
+            <img src={fingerFlow} className="Project__container__img" />
+            <Link to="/fingerFlow" className="Project__container__img-hover">
+              2D Puzzler Game
+            </Link>
+          </div>
+          <div className="Project__container__img-container">
+            <img src={raspberryPi} className="Project__container__img" />
+            <Link to="/raspberryPI" className="Project__container__img-hover">
+              RaspberryPi Datalogger
+            </Link>
           </div>
         </div>
       </section>
